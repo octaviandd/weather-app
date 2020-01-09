558621902bbb17dde09c6f4b624a9e34138a21bf
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2020-01-09 14:02:08 +0000

    added celsius option

--- src/app.js
+++ src/app.js
@@ -7,44 +7,55 @@ const toFahrenheit = document.querySelector('#fahrenheit')
 
 
 
-window.addEventListener('load', () =>{
-    let lat;
-    let long;
-
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            long = position.coords.longitude;
-            lat = position.coords.latitude
+const getData = (function(){
+    window.addEventListener('load', () =>{
+        let lat;
+        let long;
     
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(position =>{
+                long = position.coords.longitude;
+                lat = position.coords.latitude
+        
+        
+                const proxy = "http://cors-anywhere.herokuapp.com/"
+                fetch(`${proxy}https://api.darksky.net/forecast/469161b96bc38c524e0a6454c094ce6e/${lat},${long}`)
+                    .then(response =>{
+                        return response.json()
+                    })
+                    .then(data => {
+                         console.log(data)
+                    
+                    location.innerHTML = data.timezone
+                    temperature.innerHTML = Math.round(data.currently.temperature) + " °F"
     
-            const proxy = "http://cors-anywhere.herokuapp.com/"
-            fetch(`${proxy}https://api.darksky.net/forecast/469161b96bc38c524e0a6454c094ce6e/${lat},${long}`)
-                .then(response =>{
-                    return response.json()
+                    //tocelsius
+                    toCelsius.addEventListener('click', function(e){
+                        e.preventDefault()
+                        temperature.innerHTML = Math.round((data.currently.temperature - 32) * 0.55) + " °C"
+                    })
+    
+                    toFahrenheit.addEventListener('click', function(e){
+                        e.preventDefault()
+                        temperature.innerHTML = Math.round(((data.currently.temperature - 32) * 0.55) * 1.8 + 32) + " °F"
+                    })
+    
+                    forecast.innerHTML = data.hourly.summary
+                    let currentIcon = data.currently.icon.toUpperCase();
+    
+                    var skycons = new Skycons({"color": "white"});
+                    skycons.add(document.querySelector("#icon1"), currentIcon);
+    
+                    skycons.play();
                 })
-                .then(data => {
-                     console.log(data)
-                
-                location.innerHTML = data.timezone
-                temperature.innerHTML = Math.round(data.currently.temperature + " °F")
+            })  
+        }
+    }) 
+    
+})()
 
-                //tocelsius
-                toCelsius.addEventListener('click', function(){
-                    temperature.innerHTML = Math.round((data.currently.temperature - 32) * 0.55)
-                })
 
-                toFahrenheit.addEventListener('click', function(){
-                    temperature.innerHTML = Math.round(((data.currently.temperature - 32) * 0.55) * 1.8 + 32)
-                })
 
-                forecast.innerHTML = data.daily.summary
-                let currentIcon = data.currently.icon.toUpperCase();
 
-                var skycons = new Skycons({"color": "white"});
-                skycons.add(document.querySelector("#icon1"), Skycons.RAIN);
 
-                skycons.play();
-            })
-        })  
-    }
-}) 
+
